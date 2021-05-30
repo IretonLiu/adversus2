f67e73e52cac7eda52c8d817e9b6c9de6ea7f283
William Hill <whill4@me.com>
2021-05-30 22:37:26 +0200

    Added coordinate conversion functions

---
+++ src/Utils.js
@@ -0,0 +1,26 @@
+import { Vector2, Vector3 } from "three";
+
+import Constants from "./Constants";
+
+export default Utils = {
+  convertWorldToThickGrid(worldVector3) {
+    // takes in a 3-dimensional vector
+    // returns a 2-dimensional vector
+
+    return new Vector2(
+      worldVector3.x / Constants.WALL_SIZE,
+      worldVector3.z / Constants.WALL_SIZE
+    );
+  },
+
+  convertThickGridToWorld(gridVector2, height = 0) {
+    // takes in a 2-dimensional
+    // returns a 3-dimensional vector
+
+    return new Vector3(
+      gridVector2.x * Constants.WALL_SIZE,
+      height,
+      gridVector2.y * Constants.WALL_SIZE
+    );
+  },
+};
